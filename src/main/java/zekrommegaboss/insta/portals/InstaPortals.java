0dd753ee7552b2087a666932e12dbf167f90d64e
danielMoreland00 <daniel.e.moreland@gmail.com>
2021-04-05 17:24:01 -0500

    add teleport orb item

--- src/main/java/zekrommegaboss/insta/portals/InstaPortals.java
+++ src/main/java/zekrommegaboss/insta/portals/InstaPortals.java
@@ -1,14 +1,17 @@
 package zekrommegaboss.insta.portals;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class InstaPortals implements ModInitializer {
+
+	public static final Item TELEPORT_ORB = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("instaportals", "teleport_orb"), TELEPORT_ORB);
 	}
 }
