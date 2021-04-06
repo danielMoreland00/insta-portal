122acadc4761383d415b3a2b41d1f08ebfaa5e6a
danielMoreland00 <daniel.e.moreland@gmail.com>
2021-04-05 19:12:28 -0500

    add spaghetti like a champ

--- src/main/java/zekrommegaboss/insta/portals/InstaPortals.java
+++ src/main/java/zekrommegaboss/insta/portals/InstaPortals.java
@@ -17,11 +17,14 @@ public class InstaPortals implements ModInitializer {
 	public static final Item TELEPORT_ORB = new Item(new Item.Settings().group(ItemGroup.MISC));
 
 	public static final Block PORTAL_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(5.0F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES).requiresTool());
+	
+	public static final Item SPAGHETTI = new Item(new Item.Settings().group(ItemGroup.FOOD).food(Spaghetti.SPAGHETTI));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("instaportals", "teleport_orb"), TELEPORT_ORB);
 		Registry.register(Registry.BLOCK, new Identifier("instaportals", "portal_block"), PORTAL_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("instaportals", "portal_block"), new BlockItem(PORTAL_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, new Identifier("instaportals", "spaghetti"), SPAGHETTI);
 	}
 }
---
+++ src/main/java/zekrommegaboss/insta/portals/Spaghetti.java
@@ -0,0 +1,7 @@
+package zekrommegaboss.insta.portals;
+
+import net.minecraft.item.FoodComponent;
+
+public class Spaghetti {
+	public static final FoodComponent SPAGHETTI = (new FoodComponent.Builder()).hunger(10).saturationModifier(1.2f).build();
+}
